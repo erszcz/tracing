e6b2397145010ccd39ea948b64588cbfe865d391
Radek Szymczyszyn <lavrin@gmail.com>
2025-12-12 20:09:46 +0100

    Add Tidewave

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Tracing.MixProject do
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
@@ -19,16 +20,29 @@ defmodule Tracing.MixProject do
     ]
   end
 
+  def aliases do
+    [
+      tidewave:
+        "run --no-halt -e 'Agent.start(fn -> Bandit.start_link(plug: Tidewave, port: 4000) end)'"
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+
+      # Tracing
       {:erlang_doctor,
        git: "https://github.com/erszcz/erlang_doctor.git", branch: "master", override: true},
       {:ex_doctor, "~> 0.3.2"},
       {:extrace, "~> 0.6.0"},
-      {:recon, "~> 2.5"}
+      {:recon, "~> 2.5"},
+
+      # AI agent
+      {:tidewave, "~> 0.4", only: :dev},
+      {:bandit, "~> 1.0", only: :dev}
     ]
   end
 end
